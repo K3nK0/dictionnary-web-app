f99d0134f85fde75569e5f47c9978abceded0e8d
K3nK0 <uncommonmen@hotmail.fr>
2023-06-29 11:41:24 +0200

    mise en place API

--- javascript/app.js
+++ javascript/app.js
@@ -16,6 +16,49 @@ formSearch.addEventListener('submit', (e) => {
         errorEmpty.textContent = "";
         resultDisplay.textContent = "";
         notDefinition.classList.remove('active');
+        dictApiCall(input.value);
     }
 })
 
+async function dictApiCall(searchInput) {
+
+    try {
+        const response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${searchInput}`);
+
+        const data = await response.json();
+
+        // console.log(data)
+
+        if(data.title) {
+            errorEmpty.textContent = data.title;
+            return;
+        }
+        
+        createDictionnary(data);
+    }
+
+    catch(error) {
+        errorEmpty.textContent = `${error}`;
+    }
+}
+
+function createDictionnary(data) {
+
+    // const titleWord = data[0].word;
+    // const phoneticWord = data[0].phonetic;
+    const phoneticAudio = [];
+    data[0].phonetics.forEach(el => {
+        if(el.audio){
+            phoneticAudio.push(el.audio)
+        }
+    })
+
+    resultDisplay.innerHTML = `<h1>${data[0].word}</h3>
+                            <p>${data[0].phonetic}</p>`;
+
+
+    console.log(phoneticAudio);
+
+
+
+}
