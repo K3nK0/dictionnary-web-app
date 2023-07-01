cd6223bc8ce56334afd7082ca3f37660ce8ef033
K3nK0 <uncommonmen@hotmail.fr>
2023-07-01 08:44:59 +0200

    play phonetic logo

--- javascript/app.js
+++ javascript/app.js
@@ -11,6 +11,8 @@ formSearch.addEventListener('submit', (e) => {
 
     if(input.value === ""){
         errorEmpty.textContent = "Whoops, can't be empty...";
+        resultDisplay.textContent = "";
+        notDefinition.classList.remove('active');
         return;
     }else{
         errorEmpty.textContent = "";
@@ -30,7 +32,7 @@ async function dictApiCall(searchInput) {
         // console.log(data)
 
         if(data.title) {
-            errorEmpty.textContent = data.title;
+            notDefinition.classList.add('active');
             return;
         }
         
@@ -59,15 +61,32 @@ function createDictionnary(data) {
         <p>${data[0].phonetic}</p>
         <div class="player-phonetic">
             <img id="play-phonetic" src="assets/images/icon-play.svg" alt="logo play audio">
-            <audio id="sound-phonetic" src="${phoneticAudio[0]}" controls></audio>
+            <audio id="sound-phonetic" src="${phoneticAudio[0]}"></audio>
         </div>`;
 
     resultDisplay.appendChild(divPhonetic)
-    
+
+    const iconPhonetic = document.getElementById('play-phonetic');
+    const audioPhonetic = document.getElementById('sound-phonetic');
+
+    iconPhonetic.addEventListener('click', () => {
+        audioPhonetic.volume = 1;
+        audioPhonetic.play();
+    });
 
 
-    console.log(phoneticAudio[0]);
+    const blocMeanings = document.createElement('div');
+    blocMeanings.className = "container-meanings";
 
+    console.log(data[0].meanings);
 
+    data[0].meanings.forEach((e) => {
+        const cardPartOfSpeech = document.createElement('div');
+        cardPartOfSpeech.className('card-partOfSpeech');
+
+        cardPartOfSpeech.innerText = e.partOfSpeech;
+        cardPartOfSpeech.innerHTML = '<hr>';
+    })
 
 }
+
